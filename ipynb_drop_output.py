305723d1d26391c51a6bedc6b7fbda3a757d237e
Bryan W. Weber <bryan@bryanwweber.com>
2016-05-26 17:20:54 -0400

    Fix deletion of execution_count

--- ipynb_drop_output.py
+++ ipynb_drop_output.py
@@ -64,9 +64,10 @@ def strip_output_from_cell(cell):
     if "outputs" in cell:
         cell["outputs"] = []
     if "execution_count" in cell:
-        del cell["execution_count"]
+        cell["execution_count"] = None
 
 for cell in json_in["cells"]:
     strip_output_from_cell(cell)
 
 json.dump(json_in, sys.stdout, sort_keys=True, indent=1, separators=(",", ": "))
+sys.stdout.write('\n')
